5336122c124d688327f1b4f64337cf47b239897c
kingvyas <kumarvyasss@gmail.com>
2022-07-11 11:54:51 +0530

    add user modal completed

---
+++ src/lib/create-site.svelte
@@ -0,0 +1,146 @@
+<script>
+  import { onMount } from "svelte";
+  import { scale, fade, slide, fly } from "svelte/transition";
+  import { page } from "$app/stores";
+  import { showModal } from "./store";
+
+  const host = $page.url.host;
+  const user = $page.params.user;
+
+  const modalWrapAnim = {
+    duration: 200,
+  };
+
+  const modalAnim = {
+    duration: 250,
+    start: 0.6,
+  };
+
+  const handleClick = () => {
+    $showModal = false;
+  };
+</script>
+
+{#if $showModal}
+  <div transition:fade={modalWrapAnim} class="modal-wrapper">
+    <div transition:scale={modalAnim} class="modal">
+      <h3 class="title">Create a new site?</h3>
+      <div class="seperator" />
+      <p class="info">Great! This site doesn't exist, you can make it yours.</p>
+      <div class="user-info-wrapper">
+        <div class="user-info">
+          <span class="host-name">securedtext.com/</span>{user}
+        </div>
+        <div class="seperator" />
+      </div>
+      <div class="button-wrapper">
+        <button on:click={handleClick} class="secondary">Cancel</button>
+        <button class="primary">Create</button>
+      </div>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .modal-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    padding: 15px 25px;
+    width: 300px;
+    background-color: white;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .title {
+    font-weight: 500;
+    width: 100%;
+    margin-bottom: 11px;
+  }
+
+  .seperator {
+    height: 1px;
+    width: inherit;
+    background-color: rgb(223, 227, 230);
+  }
+
+  .info {
+    margin: 15px 0 20px;
+    width: 100%;
+    font-size: 14px;
+    line-height: 20px;
+    opacity: 0.9;
+  }
+
+  .user-info-wrapper {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+  }
+
+  .user-info {
+    background-color: rgb(248, 248, 248);
+    border-radius: 10px;
+    padding: 5px 10px;
+    font-size: 15px;
+    font-weight: 500;
+    word-break: break-all;
+    text-align: center;
+    letter-spacing: 1px;
+  }
+
+  .host-name {
+    opacity: 0.6;
+    font-weight: 400;
+  }
+
+  .button-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    column-gap: 5px;
+  }
+
+  button {
+    height: 35px;
+    border: none;
+    outline: none;
+    font-size: 14px;
+    font-weight: 500;
+    border-radius: 10px;
+    cursor: pointer;
+    padding: 0 15px;
+    transition: all 200ms ease-in-out;
+    will-change: transform;
+  }
+
+  button:active {
+    transform: scale(0.95);
+  }
+
+  .primary {
+    background-color: rgb(206, 228, 254);
+    color: rgb(0, 114, 245);
+  }
+
+  .secondary {
+    background-color: transparent;
+  }
+
+  .secondary:hover {
+    background-color: rgb(239, 239, 239);
+  }
+</style>
