6bf6912d247233fd8f98770e5a1599efd86b0776
kingvyas <kumarvyasss@gmail.com>
2022-07-22 12:57:56 +0530

    added footer

--- src/lib/home/footer.svelte
+++ src/lib/home/footer.svelte
@@ -1,43 +1,45 @@
 <footer>
   <div class="footer-wrapper">
-    <div class="author-wrapper">
+    <div class="email-wrapper section">
+      <p>
+        Email us:
+        <a target="_blank" href="mailto:securedtext@gmail.com">
+          securedtext@gmail.com
+        </a>
+      </p>
+    </div>
+    <div class="author-wrapper section">
       <p>
         Created by
         <a target="_blank" href="https://github.com/hrvsdev"> Harsh Vyas </a>
       </p>
-    </div>
-    <div class="inspired-wrapper">
-      <!-- <a href="https://protectdtext.com" target="_blank">
-        Inspired by Protected Text
-      </a> -->
     </div>
   </div>
 </footer>
 
 <style lang="scss">
   footer {
-    padding: 80px 35px 50px;
+    margin-top: 80px;
+    padding: 25px 35px;
+    border-top: 1px solid rgb(239 239 239);
   }
 
   .footer-wrapper {
     max-width: 985px;
     margin: auto;
     display: flex;
-    flex-direction: column;
-    row-gap: 20px;
-    justify-content: center;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
   }
 
-  .author-wrapper {
-    font-size: 17px;
+  .section {
     color: #656565;
     a {
       text-decoration: none;
       color: black;
       border-bottom: 1px solid #ababab;
       transition: border-color 300ms;
-      font-weight: 500;
 
       &:hover {
         border-bottom-color: black;
