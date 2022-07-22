8c2b98fb62533b4143a4ad67008727e1fa10d1df
kingvyas <kumarvyasss@gmail.com>
2022-07-22 14:45:17 +0530

    added some comments

--- src/lib/note/notes/editor.svelte
+++ src/lib/note/notes/editor.svelte
@@ -13,16 +13,25 @@
   import genHash from "../../../utils/genHash.util";
   import { encObj } from "../../../utils/encrypt.util";
 
+  // Title state
   let input = "";
+
+  // Textarea element
   let textarea;
 
+  // Back Click for small devices
   const onBackClick = () => {
     $isNoteOpen = false;
   };
 
+
+  // Save action
   const handleSave = async () => {
+
+    // Previous content hash
     const tempContentHash = genHash(String(JSON.stringify($notes) + $passHash));
 
+    // Data that will sent to server
     const data = {
       encContent: encObj($notes, $passHash),
       prevContentHash: $contentHash,
@@ -38,34 +47,59 @@
     }
   };
 
+  // Save button action
   const onSaveClick = () => {
+    // If note is empty delete the note
     if (!(input.value.trim() || textarea.value.trim())) return onDeleteClick();
+
+    // If a note is selected, update the note array with new data
     if ($currentNote.id) {
       $notes[$notes.indexOf($currentNote)] = {
         ...$currentNote,
         note: textarea.value,
         title: input.value,
       };
+
+      // Also, update the current selected note
       $currentNote = $notes[$currentNote.id];
     } else {
+
+      // If no note is selected, add the new note
       $notes = [
         ...$notes,
         { id: `${$notes.length}`, note: textarea.value, title: input.value },
       ];
+
+      // Change current note to recent note
       $currentNote = $notes[$notes.length - 1];
     }
+
+    // Run save function
     handleSave();
+
+    // Close editor on small devices
     $isNoteOpen = false;
   };
 
+  // Delete button action
   const onDeleteClick = () => {
+    // If there is no note selected, do nothing
     if (!$currentNote.id) return;
+
+    // Remove the current selected note from array
     $notes = $notes.filter((e, i) => i !== $notes.indexOf($currentNote));
+
+    // Remove current selected note
     $currentNote = { id: "", note: "", title: "" };
+
+    // Run  save function
     handleSave();
+
+    // Close editor
     $isNoteOpen = false;
   };
 
+  // Shortcut keys function
   const onShortcutKey = (e) => {
     if ($showModal) return;
     if (e.ctrlKey && (e.key === "s" || e.key === "S")) {
